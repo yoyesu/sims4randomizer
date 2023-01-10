0b955144bcbe97f3aec862dc8c736eddb6e4c5ec
yoyesu <yoyes15@gmail.com>
2023-01-10 10:41:41 +0000

    deleted because the corresponding controller was deleted

--- src/main/java/com/ms/sims4randomnizer/controller/forms/IndividualFormsConfig.java
+++
@@ -1,6 +0,0 @@
-package com.ms.sims4randomnizer.controller.forms;
-
-public class IndividualFormsConfig {
-
-    //when we make available customization for the individual randomizers, this will be useful. Ex. bound params
-}
