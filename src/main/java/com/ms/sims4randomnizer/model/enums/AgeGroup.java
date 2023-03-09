07387fafbc7651967aeef0b8cff62c7c76550179
yoyesu <yoyes15@gmail.com>
2023-03-09 20:06:15 +0000

    removed id property

--- src/main/java/com/ms/sims4randomnizer/model/enums/AgeGroup.java
+++ src/main/java/com/ms/sims4randomnizer/model/enums/AgeGroup.java
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AgeGroup {
-    TODDLER(1,"Toddler"), CHILD(2,"Child"), TEEN(3, "Teen"), YOUNG_ADULT(4, "Young adult"), ADULT(5, "Adult"), ELDERLY(6, "Elder");
-    public int id;
-    public String name;
+    TODDLER("Toddler"), CHILD("Child"), TEEN("Teen"), YOUNG_ADULT("Young adult"), ADULT("Adult"), ELDERLY("Elder");
 
-    AgeGroup(int id, String name){
-        this.id = id;
+    public final String name;
+
+    AgeGroup(String name){
         this.name = name;}
 }
