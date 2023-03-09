cd0de51f4ec84bd0c130c195887050377472da60
yoyesu <yoyes15@gmail.com>
2023-03-09 20:11:35 +0000

    renamed files

--- src/main/java/com/ms/sims4randomnizer/model/enums/ChildSkill.java
+++ src/main/java/com/ms/sims4randomnizer/model/enums/ChildSkill.java
@@ -3,11 +3,11 @@ package com.ms.sims4randomnizer.model.enums;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum ChildSkills {
+public enum ChildSkill {
     CREATIVITY("Creativity"), MENTAL("Mental"), MOTOR("Motor"), SOCIAL("Social");
 
     public String name;
-    ChildSkills(String name){
+    ChildSkill(String name){
         this.name = name;
     }
 }
--- src/main/java/com/ms/sims4randomnizer/model/enums/ToddlerSkill.java
+++ src/main/java/com/ms/sims4randomnizer/model/enums/ToddlerSkill.java
@@ -3,12 +3,12 @@ package com.ms.sims4randomnizer.model.enums;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum ToddlerSkills {
+public enum ToddlerSkill {
 
     COMMUNICATION("Communication"), IMAGINATION("Imagination"), MOVEMENT("Movement"), POTTY("Potty"), THINKING("Thinking");
 
     public String name;
-    ToddlerSkills(String name){
+    ToddlerSkill(String name){
         this.name = name;
     }
 }
