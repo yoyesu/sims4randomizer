ed2ffcabf3fd4fe190be9d58b527f4bf41d3e2a4
yoyesu <yoyes15@gmail.com>
2023-03-10 00:43:06 +0000

    Revert "removed id property"
    This reverts commit 07387fafbc7651967aeef0b8cff62c7c76550179.

--- src/main/java/com/ms/sims4randomnizer/model/enums/AgeGroup.java
+++ src/main/java/com/ms/sims4randomnizer/model/enums/AgeGroup.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AgeGroup {
-    TODDLER("Toddler"), CHILD("Child"), TEEN("Teen"), YOUNG_ADULT("Young adult"), ADULT("Adult"), ELDERLY("Elder");
+    TODDLER(1,"Toddler"), CHILD(2,"Child"), TEEN(3, "Teen"), YOUNG_ADULT(4, "Young adult"), ADULT(5, "Adult"), ELDERLY(6, "Elder");
+    public int id;
+    public String name;
 
-    public final String name;
-
-    AgeGroup(String name){
+    AgeGroup(int id, String name){
+        this.id = id;
         this.name = name;}
 }
